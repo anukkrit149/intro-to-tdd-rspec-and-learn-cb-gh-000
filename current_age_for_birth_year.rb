09b11e006a7a81a77473a40148bcb0a55923dc14
Anukkrit Shanker <anukkrit.179301038@muj.manipal.edu>
2017-12-19 16:08:05 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,9 +1,5 @@
 def current_age_for_birth_year(birth_year)
-  current_year = Time.now.year
-  answer = current_year - birth_year
-
-  age_of_person = current_age_for_birth_year(birth_year)
-  expect(age_of_person).to eq(answer)
+  Time.now.year-birth_year
+  
 end
-
 current_age_for_birth_year(1999)
